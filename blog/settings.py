038c59e644135cb6632dc420abd48d9b1f425bd2
Suraj Singh <suraj@localhost.localdomain>
2018-05-31 13:43:36 -0400

    Latest Update

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+from .security_keys import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(ztvyrr5zg76z3!r26$7a1aj&y!e#jyeivrk*za=_(3^sfafp7'
-
+# SECRET_KEY = '(ztvyrr5zg76z3!r26$7a1aj&y!e#jyeivrk*za=_(3^sfafp7'
+SECRET_KEY = SECURITY_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
